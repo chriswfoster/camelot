d9f301eaa9661207b524ef07713475ac868d90dc
Chris <chriswfoster@gmail.com>
2018-01-19 15:37:56 -0600

    Deleted the old mob viewer too

--- src/router.js
+++ src/router.js
@@ -3,7 +3,7 @@ import {Route, Switch} from 'react-router-dom';
 import {Provider} from 'react-redux';
 
 import Home from './components/Home/Home'
-import MobBrowser from './components/MobBrowser/MobBrowser'
+
 import ViewPlayerInv from './components/ViewPlayerInv/ViewPlayerInv.js'
 import LoginSignup from './components/LoginSignup/LoginSignup'
 import YourNode from './components/YourNode/YourNode'
@@ -17,7 +17,7 @@ export default(
     <Switch>
     <Route exact path="/" component={Home}/>
     <Route path="/viewplayersinv" component={ViewPlayerInv} />
-    <Route path="/mobbrowser" component={MobBrowser} />
+
     <Route path="/loginsignup" component={LoginSignup} />
     <Route path="/yournode" component={YourNode} />
     <Route path="/modelviewer" component={ModelViewer} />
