d8d211b99b104aeecb498a401fa8c293a14ec817
Chris <chriswfoster@gmail.com>
2018-01-18 20:11:45 -0600

    Got rid of throw errors, turned into console.logs

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -6,7 +6,7 @@ module.exports = {
     connection.query(
       `SELECT DISTINCT Name, Level FROM mob WHERE Name LIKE '%${search}%' ORDER BY Name LIMIT 100`,
       function(error, results, fields) {
-        if (error) throw error
+        if (error) console.log(error)
 
         return res.status(200).send(results)
       }
@@ -20,7 +20,7 @@ module.exports = {
     connection.query(
       `SELECT dc.Name as PlayerName, tp.Name as ItemName, nv.Count  from inventory nv JOIN itemtemplate tp ON nv.ITemplate_Id = tp.Id_nb JOIN dolcharacters dc ON dc.DOLCharacters_id = nv.OwnerID WHERE dc.Name LIKE '%${search}%' ORDER BY dc.Name asc, tp.Name LIMIT 300`,
       function(error, results, fields) {
-        if (error) throw error
+        if (error) console.log(error)
 
         return res.status(200).send(results)
       }
@@ -36,7 +36,7 @@ module.exports = {
       results,
       fields
     ) {
-      if (error) throw error
+      if (error) console.log(error)
 
       return res.status(200).send(results)
     })
