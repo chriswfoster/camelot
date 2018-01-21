70ce3c9c686354e6ba5e9f3fff876ecf1b8405bd
Chris <chriswfoster@gmail.com>
2018-01-20 18:46:15 -0600

    Added Account tool

---
+++ src/components/AccountTool/AccountTool.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class AccountTool extends Component{
+
+
+
+    render(){
+        return(
+            <div>
+                Nothing here yet.
+                </div>
+        )
+    }
+}
