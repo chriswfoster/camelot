46e6fdbdc33b5f4296edef541fb12a6a9af06fde
ChriswFoster <chriswfoster@gmail.com>
2018-02-07 23:48:46 -0600

    AccountTool page built out some

--- src/components/AccountTool/AccountTool.js
+++ src/components/AccountTool/AccountTool.js
@@ -1,13 +1,36 @@
 import React, {Component} from 'react'
-
+import { connect } from "react-redux"
+import { loadUserInfo } from "../../redux/reducer"
 class AccountTool extends Component{
+constructor(){
+    super()
+    this.state = {
 
+    }
+}
+
+componenetDidMount(){
+    
+    
+    this.props.loadUserInfo(user)
+}
 
 
     render(){
+        const {user} = this.props
+        const accountlist = user.daocaccount.map((account, i) => (
+            <p>{account}</p>
+        ))
+
         return(
             <div style={{color: 'white'}}>
-                Nothing here yet. But coming soon!
+            
+            <div>{accountlist}</div>
+            
+
+
+
+
                 </div>
         )
     }
