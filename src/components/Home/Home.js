d71603fb1cdc5200a735cdfc703851c8b230358c
ChriswFoster <chriswfoster@gmail.com>
2018-02-16 23:45:56 -0600

    removed redundancy in Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,7 +9,12 @@ class Home extends Component {
   constructor() {
     super()
     this.state = {
-      thenews: []
+      thenews: [
+        {
+          title: "No stories here",
+          postbody: "REFRESH PAGE: articles should then load",
+          postdate: ""
+    }]
     }
   }
 
@@ -21,21 +26,17 @@ class Home extends Component {
 
   render() {
     const { thenews } = this.state
-    let newsarticles
-    {
-      thenews.constructor === Array
-        ? (newsarticles = thenews.map((article, i) => (
-            <div className="newsfeedpostbox" key={i}>
-              <h3>{article.title}</h3>
-              <p>{article.postbody}</p>
-              <h6>
-                Posted <Moment fromNow>{article.postdate}</Moment>, on{" "}
-                <Moment format="MMM, DD, YYYY">{article.postdate}</Moment>.
-              </h6>
-            </div>
-          )))
-        : null
-    }
+    let newsarticles;
+   newsarticles = thenews.map((article, i) => (
+      <div className="newsfeedpostbox" key={i}>
+        <h3>{article.title}</h3>
+        <p>{article.postbody}</p>
+        <h6>
+          Posted <Moment fromNow>{article.postdate}</Moment>, on{" "}
+          <Moment format="MMM, DD, YYYY">{article.postdate}</Moment>.
+        </h6>
+      </div>
+    ))
     return (
       <div className="home">
         <header className="App-header">
@@ -47,11 +48,10 @@ class Home extends Component {
             <div>
               {" "}
               <p>
-                Hey everyone, remember to join the discord. I'll try to post
-                patch notes here as well as in discord.
+                Hey everyone, remember to join the discord. I'll try to post patch notes here as well as in discord.
               </p>
               <p>
-                Here's my discord (<b style={{ color: "lightblue" }}>TCkYBVx</b>),
+               Here's my discord (<b style={{ color: "lightblue" }}>TCkYBVx</b>),
                 you're free to drop by for a hello, drop ideas in the idea
                 channel, etc.
               </p>
